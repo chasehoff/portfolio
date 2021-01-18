32e124ce026aa31de3d6b1dd8b46dd04d86add0f
chasehoff <crhoffman@student.fullsail.edu>
2021-01-17 20:55:57 -0500

    Added media queries for home page

--- src/views/contact/Contact.js
+++ src/views/contact/Contact.js
@@ -22,12 +22,12 @@ function Contact() {
             <h1 className="contact__header">Contact</h1>
             <form className="contact__form" onSubmit={handleSubmit}>
                 <div className="contact__input__section">
-                    <input className="contact__item__double" name="fname" placeholder="First Name" value={formInfo.fname} onChange={(e) => setFormInfo({...formInfo, fname: e.target.value})} />
-                    <input className="contact__item__double" name="lname" placeholder="Last Name" value={formInfo.lname} onChange={(e) => setFormInfo({...formInfo, lname: e.target.value})} />
+                    <input required className="contact__item__double" name="fname" placeholder="First Name*" value={formInfo.fname} onChange={(e) => setFormInfo({...formInfo, fname: e.target.value})} />
+                    <input required className="contact__item__double" name="lname" placeholder="Last Name*" value={formInfo.lname} onChange={(e) => setFormInfo({...formInfo, lname: e.target.value})} />
                 </div>
                 <div className="contact__input__section">
-                    <input className="contact__item__double" name="email" placeholder="Email" value={formInfo.email} onChange={(e) => setFormInfo({...formInfo, email: e.target.value})} />
-                    <input className="contact__item__double" name="company" placeholder="Company" value={formInfo.company} onChange={(e) => setFormInfo({...formInfo, company: e.target.value})} />
+                    <input required className="contact__item__double" name="email" placeholder="Email*" value={formInfo.email} onChange={(e) => setFormInfo({...formInfo, email: e.target.value})} />
+                    <input required className="contact__item__double" name="company" placeholder="Company" value={formInfo.company} onChange={(e) => setFormInfo({...formInfo, company: e.target.value})} /> 
                 </div>
                 <div className="contact__input__section">
                     <textarea rows="8" className="contact__item__single" name="message" placeholder="Message..." value={formInfo.message} onChange={(e) => setFormInfo({...formInfo, message: e.target.value})} />
