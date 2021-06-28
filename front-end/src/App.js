13eddb5db1f4790441ea00c8ade3f0384143141b
chasehoff <crhoffman@student.fullsail.edu>
2021-06-28 00:33:01 -0400

    Completed the mobile nav

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,6 +15,12 @@ import TimeGuru from './views/projects/timeguru/TimeGuru';
 import FoamSolutions from './views/projects/foamsolutions/FoamSolutions';
 import NorthernContracting from './views/projects/northerncontracting/NorthernContracting';
 import Drawer from '@material-ui/core/Drawer';
+import Logo from './images/main_logo.png';
+import { IoMdClose } from 'react-icons/io';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import HomeIcon from '@material-ui/icons/Home';
+import ViewCarouselIcon from '@material-ui/icons/ViewCarousel';
+import { Link } from 'react-router-dom';
 
 function App() {
   const [ isOpen, setOpen ] = useState(false);
@@ -22,9 +28,26 @@ function App() {
     <Router>
       <div className="app">
         <Navigation setOpen={setOpen} isOpen={isOpen} />
-        <Drawer anchor="right" open={isOpen} onClose={() => setOpen(!isOpen)}>
-            <p>Test</p>
-          </Drawer>
+        <Drawer className="mobile__nav__container" anchor="right" open={isOpen} onClose={() => setOpen(!isOpen)}>
+          <div id="mobile__section__one" className="mobile__nav__section">
+            <img className="mobile__nav__logo" src={Logo} alt="Company logo" />
+            <IoMdClose onClick={() => setOpen(false)} className="mobile__nav__close" />
+          </div>
+          <div className="mobile__nav__section">
+            <Link to="/" className="mobile__nav__link" onClick={() => setOpen(!isOpen)}>
+              <HomeIcon className="mobile__nav__link__icon"/>
+              <p className="mobile__nav__link__text">Home</p>
+            </Link>
+            <Link to="/projects" className="mobile__nav__link" onClick={() => setOpen(!isOpen)}>
+              <ContactMailIcon className="mobile__nav__link__icon"/>
+              <p className="mobile__nav__link__text">Projects</p>
+            </Link>
+            <Link to="/contact" className="mobile__nav__link" onClick={() => setOpen(!isOpen)}>
+              <ViewCarouselIcon className="mobile__nav__link__icon"/>
+              <p className="mobile__nav__link__text">Contact</p>
+            </Link>
+          </div>
+        </Drawer>
         <Hero />
         <Switch>
           <Route exact path="/" component={Home} />
